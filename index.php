86e516c05f32f0d7e3b92aea50adc5e4d46c308a
rmbarnard93 <rmbarnard93@gmail.com>
2020-05-12 12:52:43 +0200

    Changed entry

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
-<?php
-include "./main.html"
+<!DOCTYPE html> 
+<html>
+    <title>Richard Barnard Music</title>
+<head>
+
+</head>
+    <body>
+
+    <h1> Richard Barnard Music </h1>
+
+    </body>
+</html>
