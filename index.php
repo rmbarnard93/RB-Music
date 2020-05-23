7ded3599a4cf43896857a7441f4a7393521c625e
rmbarnard93 <rmbarnard93@gmail.com>
2020-05-23 22:14:33 +0200

    Edited MAin Content

--- index.php
+++ index.php
@@ -1,16 +1,14 @@
-<!DOCTYPE html> 
-<html>
-    <title>Richard Barnard Music</title>
-<head>
-<link rel="stylesheet" href="/resources/bootstrap-4.4.1-dist/css/bootstrap.min.css">
-</head>
-    <body>
+<?php
 
-    <h1> Richard Barnard Music </h1>
-    <script
-  src="https://code.jquery.com/jquery-3.5.1.min.js"
-  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
-  crossorigin="anonymous"></script>
-<script src="/resources/bootstrap-4.4.1-dist/js/bootstrap.min.js"></script>
-    </body>
-</html>
+function route($path, $view){
+    $base = "projects/rbmusic";
+    $request = $_SERVER["REQUEST_URI"];
+    $url = trim(str_replace("#", "", $request));
+    $url = str_replace($base, "", $url);
+    $url = str_replace("//", "/", $url);
+    if($url === $path){
+        require __DIR__."/views/".$view;
+    }
+}
+
+route("/", "index.html");
---
+++ test.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html> 
+<html>
+    <head>
+        <title>Richard Barnard Music</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
